472863b27f5611eb2b2014e3efcb6941b05194db
Yury Sinkevich <sinkevichyv@gmail.com>
2024-04-27 18:28:02 +0300

    create controller for Operation entity

---
+++ src/main/java/by/sinkevich/demo/account/service/web/OperationResource.java
@@ -0,0 +1,25 @@
+package by.sinkevich.demo.account.service.web;
+
+import by.sinkevich.demo.account.service.service.OperationService;
+import by.sinkevich.demo.account.service.service.dto.OperationDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/operations")
+public class OperationResource {
+    private final OperationService operationService;
+
+    public OperationResource(OperationService operationService) {
+        this.operationService = operationService;
+    }
+
+    @PostMapping("")
+    public ResponseEntity<OperationDTO> save(@RequestBody OperationDTO operationDTO) {
+        return ResponseEntity.ok(operationService.save(operationDTO));
+    }
+
+}
