63b23728c31976a24ca5a57a0554212f9e68a47c
Yury Sinkevich <sinkevichyv@gmail.com>
2024-04-30 00:13:05 +0300

    remove unused variable

--- src/test/java/by/sinkevich/demo/account/service/service/OperationServiceImplTest.java
+++ src/test/java/by/sinkevich/demo/account/service/service/OperationServiceImplTest.java
@@ -15,7 +15,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
@@ -39,9 +38,6 @@ class OperationServiceImplTest {
     private ClientService clientService;
     @Autowired
     private AccountRepository accountRepository;
-
-    @LocalServerPort
-    private Integer port;
 
     static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(
             "postgres:11"
